b7cabc6eb7c3930c60d077a7f845a6419cfd1b6b
Gokul Menon <gokul.menon@free-now.com>
2023-11-15 23:21:58 +0100

    added concurrency to endpoint

--- src/main/kotlin/controller/UrlController.kt
+++ src/main/kotlin/controller/UrlController.kt
@@ -1,11 +1,12 @@
 package controller
 
 import io.swagger.annotations.ApiOperation
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.scheduling.annotation.Async
 import org.springframework.web.bind.annotation.*
 import service.UrlService
+import springfox.documentation.annotations.Cacheable
 import java.net.URI
 
 @RestController
@@ -14,13 +15,15 @@ class UrlController(private val urlService: UrlService) {
 
     @ApiOperation(value = "Convert new url", notes = "Converts long url to short url")
     @PostMapping("create-short")
+    @Async
     fun convertToShortUrl(@RequestBody request: String): String {
         return urlService.convertToShortUrl(request)
     }
 
     @ApiOperation(value = "Redirect", notes = "Finds original url from short url and redirects")
     @GetMapping("{shortUrl}")
-    fun getAndRedirect(@PathVariable shortUrl: String): ResponseEntity<Void> {
+    @Cacheable("originalUrls")
+    fun getOriginalUrl(@PathVariable shortUrl: String): ResponseEntity<Void> {
         val url = urlService.getOriginalUrl(shortUrl)
         return ResponseEntity.status(HttpStatus.FOUND)
             .location(URI.create(url))
